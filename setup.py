a88c84aed0acb87d11e0cc9d0c144903ed0ee849
Rafael Rodrigues <rafael.rafarod@gmail.com>
2021-05-18 19:07:42 -0300

    Link version with setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README_pypi.md'), encoding='utf-8') as f:
 out = subprocess.Popen(['python', path.join(this_directory,'version.py')], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 stdout, _ = out.communicate()
 version = str(stdout)
-print(version.decode("utf-8").strip())
+print(version.strip())
 
 packages = \
 ['pytrendseries']
