dbdb961c7b03074cc50de3b25370a17acc8a654f
RAFAEL RODRIGUES <rafael.rafarod@gmail.com>
2025-02-01 11:24:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 
+PACKAGE = "pytrendseries"
+
 with open(path.join(this_directory, 'README_pypi.md'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -13,8 +15,11 @@ stdout, _ = out.communicate()
 version = stdout.decode("utf-8").strip()
 print(version)
 
+package_dir = \
+{'': 'src'}
+
 packages = \
-['pytrendseries']
+[PACKAGE]
 
 package_data = \
 {'': ['*'],
@@ -29,7 +34,7 @@ extras_require = \
  ':python_version >= "3.10"': ['matplotlib>=3.10.0', 'pandas>=2.2.3']}
 
 setup_kwargs = {
-    'name': 'pytrendseries',
+    'name': PACKAGE,
     'version': version,
     'description': 'Detect trend in time series.',
     'long_description':long_description,
@@ -38,9 +43,11 @@ setup_kwargs = {
     'author_email': 'rafael.rafarod@gmail.com',
     'maintainer': None,
     'maintainer_email': None,
-    'url': None,
+    'url': f"https://github.com/rafa-rod/{PACKAGE}",
+    'package_dir': package_dir,
     'packages': packages,
     'package_data': package_data,
+    'extras_require': extras_require,
     'install_requires': install_requires,
     'python_requires': '>=3.8,<4.0',
 }
